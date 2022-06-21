d0cf57c9b767ac7d6620d4f7a3514dce80a42806
sskong777 <sskong777@gmail.com>
2022-06-22 00:35:52 +0900

    put

---
+++ put/src/main/java/com/example/put/PutApiController.java
@@ -0,0 +1,19 @@
+package com.example.put;
+
+
+import com.example.put.dto.PutRequestDto;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PutApiController {
+
+    @PutMapping("/put")
+    public PutRequestDto put(@RequestBody PutRequestDto requestDto){
+        System.out.println(requestDto);
+        return requestDto;
+    }
+}
